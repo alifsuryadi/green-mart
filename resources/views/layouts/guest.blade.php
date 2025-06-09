21fe080f70a407e2357b5c4205c32e93cd439100
Alif Suryadi <alifsuryadi037@gmail.com>
2025-06-09 17:08:26 +0700

    fix(ui): update navigation text to 'Back to Login'

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -160,9 +160,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <!-- Back to Home Link -->
-        <a href="/" class="back-home-link">
+        <a href="/login" class="back-home-link">
             <i class="fas fa-arrow-left"></i>
-            Back to Home
+            Back to Login
         </a>
         
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">

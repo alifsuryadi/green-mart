cb2a3a25cfde37d98c4bcb18e76cfbe6db6b4ff0
Alif Suryadi <alifsuryadi037@gmail.com>
2025-06-09 17:06:51 +0700

    fix: correct minor bug in guest title and favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Green Mart') }}</title>
+        <title>{{ config('APP.NAME', 'Green Mart') }}</title>
+        <link rel="icon" href="{{ asset('/favicomatic/favicon.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
